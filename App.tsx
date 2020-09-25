02fc2951550800ca21d6005fdc66239a8f576574
Tom Hacohen <tom@stosb.com>
2020-09-25 10:45:41 +0300

    Change the app's structure to be more like the rest of our apps.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,2 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Index from "./src/Index";
+export default Index;
