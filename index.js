d08fe1149b0081807374520ba72b997c7c1af3b9
Tom Hacohen <tom@stosb.com>
2020-10-02 15:06:59 +0300

    Remove expo dependency as it was pulling in a lot of unneeded stuff.
    And remove the permission blacklist as it's not longer needed.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { registerRootComponent } from 'expo';
+import { AppRegistry } from "react-native";
 
-import App from './App';
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent("main", () => App);
