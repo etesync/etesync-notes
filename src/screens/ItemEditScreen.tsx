etesync/etesync-notes

commit 88ad188654d801d615a1c2ed9490742b700752c0

ItemEdit: fix scrolling when keyboard is open to not hide content.
Fixes #26

src/screens/ItemEditScreen.tsx
@@ -292,10 +292,7 @@ function TextEditor(props: TextEditorPropsType) {
   const theme = useTheme();
 
   return (
-    <KeyboardAvoidingView
-      behavior="height"
-      style={[{ backgroundColor: theme.colors.background }, props.style]}
-    >
+    <>
       <RawTextInput
         textAlignVertical="top"
         multiline
@@ -304,6 +301,10 @@ function TextEditor(props: TextEditorPropsType) {
         onChangeText={setContent}
         value={content}
       />
-    </KeyboardAvoidingView>
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={[{ backgroundColor: theme.colors.background }, props.style]}
+      />
+    </>
   );
 }

commit 68aaee7e6ccbcc80c6b70c5665f8ea3f1ecb8f3e

ItemEdit: fix the background of the text edit to use the theme.
We broke it in 88ad188654d801d615a1c2ed9490742b700752c0.

src/screens/ItemEditScreen.tsx
@@ -297,7 +297,7 @@ function TextEditor(props: TextEditorPropsType) {
         textAlignVertical="top"
         multiline
         scrollEnabled
-        style={{ flexGrow: 1, backgroundColor: "transparent" }}
+        style={{ flexGrow: 1 }}
         onChangeText={setContent}
         value={content}
       />

src/widgets/RawTextInput.tsx
@@ -11,7 +11,7 @@ export default function RawTextInput(props_: React.ComponentProps<typeof TextInp
 
   return (
     <TextInput
-      style={[{ color: theme.colors.text, backgroundColor: theme.colors.surface }, style]}
+      style={[{ color: theme.colors.text, backgroundColor: theme.colors.background }, style]}
       {...props}
     />
   );