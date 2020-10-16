1c70cfba4b673740dec71fdd56f0bda609eb67e4
Tom Hacohen <tom@stosb.com>
2020-10-16 15:40:03 +0300

    Expo-updates (iOS only): fix URL and update deploy script
    We don't use expo-updates for Android, because there the turnaround for
    updates is quick enough to not need it.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 set -e
 
+yarn lint --max-warnings 0
+yarn tsc
+
+
 SSH_HOST=notes.etesync.com
 SSH_PORT=22
 SSH_USER=etesync
@@ -12,3 +16,22 @@ export INLINE_RUNTIME_CHUNK=false
 yarn expo build:web
 
 rsync -e "ssh -p ${SSH_PORT}" -P --delete -rvc -zz ${OUTPUTDIR}/ ${SSH_USER}@${SSH_HOST}:${SSH_TARGET_DIR}
+
+
+# Deploy the expo build
+SSH_HOST=expo.etesync.com
+SSH_PORT=22
+SSH_USER=etesync
+SSH_TARGET_DIR=sites/expo.etesync.com
+
+OUTPUTDIR='dist'
+
+PUBLIC_URL=https://expo.etesync.com
+DEPLOY_PATH='etesync-notes/release'
+# DEPLOY_PATH='test'
+
+APP_VERSION=1
+
+rm -rf "$OUTPUTDIR"
+yarn run expo export --dump-sourcemap --public-url ${PUBLIC_URL}/${DEPLOY_PATH}/${APP_VERSION}
+rsync -e "ssh -p ${SSH_PORT}" -P --delete -rvzc ${OUTPUTDIR}/ ${SSH_USER}@${SSH_HOST}:${SSH_TARGET_DIR}/${DEPLOY_PATH}/${APP_VERSION}
