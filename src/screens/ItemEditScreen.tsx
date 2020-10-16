etesync/etesync-notes

commit 88ad188654d801d615a1c2ed9490742b700752c0

ItemEdit: fix scrolling when keyboard is open to not hide content.
Fixes #26

src/screens/ItemEditScreen.tsx
@@ -292,10 +292,7 @@ function TextEditor(props: TextEditorPropsType) {
   const theme = useTheme();
 
   return (
-    <KeyboardAvoidingView
-      behavior="height"
-      style={[{ backgroundColor: theme.colors.background }, props.style]}
-    >
+    <>
       <RawTextInput
         textAlignVertical="top"
         multiline
@@ -304,6 +301,10 @@ function TextEditor(props: TextEditorPropsType) {
         onChangeText={setContent}
         value={content}
       />
-    </KeyboardAvoidingView>
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={[{ backgroundColor: theme.colors.background }, props.style]}
+      />
+    </>
   );
 }

commit 416285aca89183d7665c6427fa3d5996ffe0e35e

Web: fix display of item edit when to be max height.
Broken in 88ad188654d801d615a1c2ed9490742b700752c0

src/screens/ItemEditScreen.tsx
@@ -3,7 +3,7 @@
 
 import * as React from "react";
 import * as Etebase from "etebase";
-import { View, ViewProps, KeyboardAvoidingView } from "react-native";
+import { View, ViewProps, KeyboardAvoidingView, Platform } from "react-native";
 import { Appbar, Paragraph, useTheme } from "react-native-paper";
 import { useNavigation, RouteProp } from "@react-navigation/native";
 import { useDebouncedCallback } from "use-debounce";
@@ -303,7 +303,7 @@ function TextEditor(props: TextEditorPropsType) {
       />
       <KeyboardAvoidingView
         behavior="padding"
-        style={[{ backgroundColor: theme.colors.background }, props.style]}
+        style={[{ backgroundColor: theme.colors.background }, props.style, (Platform.OS === "web") && { display: "none" }]}
       />
     </>
   );