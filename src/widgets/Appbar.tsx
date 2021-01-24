358a5b3658063b959d93f1d3e1314fb37e611cfe
Kévin Commaille <zecakeh@pm.me>
2021-01-24 11:57:24 +0100

    Use custom Appbar
    Allows us to always provide the menu as fallback, especially useful on Web.

---
+++ src/widgets/Appbar.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { StackHeaderProps } from "@react-navigation/stack";
+import { Appbar as PaperAppbar } from "react-native-paper";
+
+import MenuButton from "./MenuButton";
+
+export default function Appbar(props: StackHeaderProps & { menuFallback: boolean }) {
+  const { insets, menuFallback, navigation, previous, scene } = props;
+  const { options } = scene.descriptor;
+  const title = options.headerTitle ?? options.title ?? scene.route.name;
+  let left: React.ReactNode = null;
+  if (options.headerLeft) {
+    left = options.headerLeft({});
+  } else if (previous) {
+    left = <PaperAppbar.BackAction onPress={navigation.goBack} />;
+  } else if (menuFallback) {
+    left = <MenuButton />;
+  }
+  const right = options.headerRight?.({});
+
+  return (
+    <PaperAppbar.Header statusBarHeight={insets.top}>
+      {left}
+      <PaperAppbar.Content title={title} />
+      {right}
+    </PaperAppbar.Header>
+  );
+}
