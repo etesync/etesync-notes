74372c0f252cad098a4e0604328480bb24ea2635
Kévin Commaille <zecakeh@pm.me>
2021-02-11 10:44:45 +0100

    Add Link to About items

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -2,11 +2,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 import * as React from "react";
-import { Linking, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import { Text, List, TouchableRipple } from "react-native-paper";
 
 import { Title } from "../widgets/Typography";
 import Container from "../widgets/Container";
+import Link from "../widgets/Link";
 
 import { expo } from "../../app.json";
 import * as C from "../constants";
@@ -22,12 +23,18 @@ function generateRenderLicenseItem(pkgLicenses: any) {
     const description = (publisher && (publisher.toLowerCase() !== pkgName.toLowerCase())) ? `${pkg.licenses} by ${publisher}` : pkg.licenses;
     const link = repository ?? url;
     return (
-      <List.Item
-        key={pkgName}
-        title={pkgName}
-        description={description}
-        right={(props) => (<List.Icon {...props} icon="chevron-right" />)}
-        onPress={link && (() => { Linking.openURL(link) })}
+      <Link
+        to={link}
+        external
+        renderChild={(props) => (
+          <List.Item
+            {...props}
+            key={pkgName}
+            title={pkgName}
+            description={description}
+            right={(props) => (<List.Icon {...props} icon="chevron-right" />)}
+          />
+        )}
       />
     );
   };
@@ -42,9 +49,15 @@ export default function AboutScreen() {
       ListHeaderComponent={() => (
         <Container>
           <Title style={{ textAlign: "center" }}>{C.appName} {expo.version}</Title>
-          <TouchableRipple onPress={() => { Linking.openURL(C.homePage) }}>
-            <Text style={{ textAlign: "center", color: theme.colors.accentText, textDecorationLine: "underline", margin: 10 }}>{C.homePage}</Text>
-          </TouchableRipple>
+          <Link
+            to={C.homePage}
+            external
+            renderChild={(props) => (
+              <TouchableRipple {...props}>
+                <Text style={{ textAlign: "center", color: theme.colors.accentText, textDecorationLine: "underline", margin: 10 }}>{C.homePage}</Text>
+              </TouchableRipple>
+            )}
+          />
           <Title style={{ marginTop: 30 }}>Open Source Licenses</Title>
         </Container>
       )}
